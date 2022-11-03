ba541b0fd80a33d69e5983bc43db31d9f6226435
hibiki73 <hibiki73@gmail.com>
2022-11-03 12:28:03 -0400

    re-dash

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,7 +1,25 @@
 <template>
   <q-page padding>
-    <div>
+    <!-- <div>
       <h1>Dashboard</h1>
+    </div> -->
+    <!-- <div class="row q-col-gutter-sm q-pa-sm"> -->
+    <div class="col-md-12 col-xs-12">
+      <q-card class="image-dashboard">
+        <q-img src="~/assets/images/graph_bar_color.gif">
+          <div class="absolute-bottom text-subtitle2 text-center">
+            NO HAY DATOS PARA MOSTRAR EN LAS GRÁFICAS
+          </div>
+        </q-img>
+      </q-card>
     </div>
+    <!-- </div> -->
   </q-page>
 </template>
+
+<style lang="sass" scoped>
+.image-dashboard
+  // height: 90%
+  // max-height: 300px
+  width: 97%
+</style>
