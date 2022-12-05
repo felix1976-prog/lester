fc95d11c251579b1a45287bdb98821bd227ac3b5
Many92 <manycuba92@gmail.com>
2022-12-04 19:55:56 -0500

    add eliminar

--- src/stores/utils/utils-store.ts
+++ src/stores/utils/utils-store.ts
@@ -1,4 +1,6 @@
 import { defineStore } from 'pinia';
+import { Notify } from 'quasar';
+import { api } from 'src/boot/axios';
 
 export const useUtilsStore = defineStore('utils', {
   state: () => ({
@@ -26,6 +28,26 @@ export const useUtilsStore = defineStore('utils', {
     },
     eliminarToggle() {
       this.isEliminarOpen = !this.isEliminarOpen;
+    },
+    async eliminar(title: string, url: string) {
+      try {
+        const { data } = await api.delete(url);
+        if (data.statusCode !== 400)
+          Notify.create({
+            icon: 'las la-fire-alt',
+            position: 'top-right',
+            message: 'Registro eliminado',
+            color: 'info',
+          });
+        return data;
+      } catch (e: any) {
+        Notify.create({
+          icon: 'las la-exclamation-triangle',
+          position: 'bottom',
+          message: `${e.message}`,
+          color: 'negative',
+        });
+      }
     },
   },
 });
