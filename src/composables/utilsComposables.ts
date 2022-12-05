0b680c315eff0280379a62acd763f092935863cb
Many92 <manycuba92@gmail.com>
2022-12-05 17:50:53 -0500

    fix delete

--- src/composables/utilsComposables.ts
+++ src/composables/utilsComposables.ts
@@ -1,11 +1,23 @@
 import { storeToRefs } from 'pinia';
+import { useUsersStore } from 'src/stores/users/users-store';
 import { useUtilsStore } from 'src/stores/utils/utils-store';
 
 export const useUtilsComposables = () => {
-  const { setToggleLogin, toggleSideMenu, eliminarToggle, addUserToggle } =
-    useUtilsStore();
+  const {
+    setToggleLogin,
+    toggleSideMenu,
+    eliminarToggle,
+    addUserToggle,
+    eliminar,
+  } = useUtilsStore();
   const { isOpenLogin, isSideMenuOpen, isAddUserOpen, isEliminarOpen } =
     storeToRefs(useUtilsStore());
+  const { getAllUsers } = useUsersStore();
+
+  const deleteComponent = async (titulo: string, url: string) => {
+    await eliminar(titulo, url);
+  };
+
   return {
     //login Form
     // state
@@ -15,10 +27,12 @@ export const useUtilsComposables = () => {
     // eliminar
     isEliminarOpen,
     eliminarToggle,
+    deleteComponent,
 
     // users
     isAddUserOpen,
     addUserToggle,
+    getAllUsers,
 
     //drawer
     isSideMenuOpen,
